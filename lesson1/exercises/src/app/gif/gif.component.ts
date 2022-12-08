de07736c077109526589143d77e54e273ec3304d
mkilp91 <mitchellakilpatrick@gmail.com>
2022-12-08 10:14:41 -0600

    completed lesson 1 exercises

---
+++ lesson1/exercises/src/app/gif/gif.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gif',
+  templateUrl: './gif.component.html',
+  styleUrls: ['./gif.component.css']
+})
+export class GifComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
