b3727ca022ed402bfa17ea3e0373a252bc083484
mkilp91 <mitchellakilpatrick@gmail.com>
2022-12-09 23:17:23 -0600

    completed selected crew column

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -23,6 +23,13 @@ export class CandidatesComponent implements OnInit {
   ngOnInit() {
   }
 
+  addToCrew(candidate: object){
+    if (!this.crew.includes(candidate)){
+      this.crew.push(candidate);
+      // console.log(this.crew);
+    }
+  }
+
   // Code the addToCrew function here:
 
 
